be277f54740f85576a1bd9b354bb74046e1d9c5e
Shreya Mishra <143825321+shrexxs@users.noreply.github.com>
2024-08-26 14:13:33 +0530

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,13 +4,14 @@ export const Hero =() => {
         <div className="bg-[#837930] text-white text-center py-2">
           <p>To know more about the AI, click <a href="#" className="underline">HERE</a></p>
           </div>
-          <div className="relative ">
+           <div className="relative ">
          <video 
-              className="w-full h-auto max-h-[500px] mx-auto mb-8" 
+              className="w-full h-auto max-h-[500px] mx-auto mb-8 object-cover" 
             src="/museo.mp4" 
             autoPlay 
             loop 
           />
+          </div>
           </div>
           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <h1 className="text-white text-6xl font-extrabold uppercase tracking-wider">MuseoMate</h1>
