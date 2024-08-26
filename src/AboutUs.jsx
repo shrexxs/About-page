ffeddafeff6b8d9c9965037cf9164de68e6b6902
Shreya Mishra <143825321+shrexxs@users.noreply.github.com>
2024-08-26 13:58:09 +0530

    Update AboutUs.jsx

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,14 +1,14 @@
 
 export const AboutUs=() => {
   return (
-    <section className="bg-white py-12">
-    <div className="container mx-auto px-4">
-    <h2 className="text-2xl font-bold mb-6 text-center border-b pb-2">ABOUT US</h2>
+  <section className="bg-white py-16">
+    <div className="container mx-auto px-8">
+    <h2 className="text-4xl font-bold mb-6 text-center" >ABOUT US</h2>
     <div className="text-center max-w-3xl mx-auto">
-    <p className="mb-4">
+    <p className="text-lg leading-relaxed">
     Welcome to <span className="font-bold">MUSEOMATE</span>, a hub where history, art, and culture come to life. Our mission is to provide an accessible and immersive experience for all. Whether you're a history enthusiast, an art lover, or a curious explorer, our collections offer something for everyone. Our state-of-the-art facilities, interactive displays, and knowledgeable staff ensure that your visit is both enjoyable and enlightening.
     </p>
-    <p>
+    <p className="text-lg leading-relaxed">
     Through this platform, you can easily browse our exhibits, purchase tickets, and plan your visit. We are committed to making art and history accessible to all, with a seamless booking experience and an inclusive environment that welcomes people from all walks of life.
     </p>
     </div>
