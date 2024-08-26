de31a2725beb368fc116d2113b4fae85b60b9e6b
Shreya Mishra <143825321+shrexxs@users.noreply.github.com>
2024-08-26 13:56:46 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,16 @@
 export const Header = () =>{
     return (
-      <header className="bg-[#311e1b] text-white p-4">
+      <header className="bg-[#311e1b] text-white p-4 ">
         <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-xl font-bold">National Council of Museums</h1>
+          <h1 className="text-white text-xl font-bold italic">National Council of Museums</h1>
           <nav>
-            <ul className="flex space-x-4">
-              <li>HOME</li>
-              <li className="text-yellow-500">ABOUT US</li>
-              <li>MUSEUMS</li>
-              <li>CONTACT</li>
+           <ul className="flex space-x-4">
+                <li><a href="#" className="text-white">HOME</a></li>
+               <li><a href="#" className="text-[#d4a919]">ABOUT US</a></li>
+                <li><a href="#" className="text-white">MUSEUMS</a></li>
+               <li><a href="#" className="text-white">CONTACT</a></li>
             </ul>
+
           </nav>
         </div>
       </header>
